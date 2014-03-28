e96acae95226dac127cfd0f6bbac5e3118694dfe
Chen Yang <chenyang@masols.com>
2014-03-28 12:06:15 +0800

    table with images

--- docbook2odf/conv_all.sh
+++ docbook2odf/conv_all.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+#
+# origin examples from docbook2odf
+#
+
 docbook2odf -f --params generate.meta=0 -o media.odt media.docbook
+docbook2odf -f --params generate.meta=0 -o table.odt table.docbook
+
+#
+# examples create by spolo
+#
+
+docbook2odf -f --params generate.meta=0 -o table.media.odt table.media.docbook
