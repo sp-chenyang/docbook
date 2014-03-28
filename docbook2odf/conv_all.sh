403176aa4796d55be5d64a28fe2b2e63a538cf4a
Chen Yang <chenyang@masols.com>
2014-03-28 13:20:00 +0800

    section example

--- docbook2odf/conv_all.sh
+++ docbook2odf/conv_all.sh
@@ -6,6 +6,8 @@
 
 docbook2odf -f --params generate.meta=0 -o media.odt media.docbook
 docbook2odf -f --params generate.meta=0 -o table.odt table.docbook
+docbook2odf -f --params generate.meta=0 -o book.odt book.docbook
+docbook2odf -f --params generate.meta=0 -o section.odt section.docbook
 
 #
 # examples create by spolo
