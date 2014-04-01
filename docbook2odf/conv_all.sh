b0d6b380f4352d0ae0c7ac2a04ea622fb9c26631
Chen Yang <chenyang@masols.com>
2014-04-01 18:57:49 +0800

    test

--- docbook2odf/conv_all.sh
+++ docbook2odf/conv_all.sh
@@ -10,7 +10,15 @@ docbook2odf -f --params generate.meta=0 -o book.odt book.docbook
 docbook2odf -f --params generate.meta=0 -o section.odt section.docbook
 
 #
+# other example
+#
+
+#http://www.filewatcher.com/p/docbook2odf-0.244-1.el4.rf.noarch.rpm.621815/usr/share/docbook2odf/tests/tests/toc.001.xml.html
+#docbook2odf -f --params generate.meta=0 -o toc.odt toc.docbook
+
+#
 # examples create by spolo
 #
 
 docbook2odf -f --params generate.meta=0 -o table.media.odt table.media.docbook
+docbook2odf -f --params generate.meta=0 -o section.link.odt section.link.docbook
